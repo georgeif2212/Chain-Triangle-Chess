4bfb29952b17f5ac895bb78d6a1bb245ad0b9194
georgeif2212 <jinfante2212@gmail.com>
2024-11-05 13:03:55 -0600

    feat: receives onClick function as a props to handle when a vertex is selected

--- src/components/Vertex.jsx
+++ src/components/Vertex.jsx
@@ -1,22 +1,20 @@
 import { Circle } from "react-konva";
 
-const Vertex = ({ x, y }) => {
-
+const Vertex = ({ x, y, onClick }) => {
   return (
     <Circle
       x={x}
       y={y}
-      radius={10} // Tamaño del vértice
-      fill="black" // Color de los vértices
-      stroke="black" // Color del borde
+      radius={10}
+      fill="black"
+      stroke="black"
       strokeWidth={1}
+      onClick={onClick} 
       onMouseEnter={(e) => {
-        // Cambia el cursor a "pointer" al pasar el mouse
         const container = e.target.getStage().container();
         container.style.cursor = "pointer";
       }}
       onMouseLeave={(e) => {
-        // Vuelve el cursor a su estilo predeterminado al salir
         const container = e.target.getStage().container();
         container.style.cursor = "default";
       }}
