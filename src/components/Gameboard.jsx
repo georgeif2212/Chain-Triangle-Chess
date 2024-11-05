42c4ee87d81c8797d1e5178080aa7acfc3efab3c
georgeif2212 <jinfante2212@gmail.com>
2024-11-05 12:26:26 -0600

    feat: the logic behind generates coordinates has been moved to the component VertexGrid

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Stage, Layer, RegularPolygon } from "react-konva";
 import Vertex from "./Vertex.jsx";
+import VertexGrid from "./VertexGrid.jsx";
 
 const GameBoard = () => {
   const [stageWidth, setStageWidth] = useState(window.innerWidth);
@@ -11,34 +12,19 @@ const GameBoard = () => {
   const radius = window.innerWidth / 4;             // * Radius of the hexagon
   const vertexSpacing = window.innerWidth / 16;     // * Spacing between vertices
   const rows = [4, 5, 6, 7, 6, 5, 4];               // * Number of vertices in each row
-  const vertices = [];                              // * Store axes of the vertices
-
-  // Genera las posiciones de los vértices en forma de hexágono entrelazado
-  rows.forEach((numVertices, rowIndex) => {
-    const yOffset =
-      polygonY + ((rowIndex - 3) * vertexSpacing * Math.sqrt(3)) / 2; // Ajuste en Y
-    const xOffsetStart = polygonX - ((numVertices - 1) * vertexSpacing) / 2; // Ajuste en X inicial
-
-    for (let i = 0; i < numVertices; i++) {
-      const x = xOffsetStart + i * vertexSpacing;
-      const y = yOffset;
-      vertices.push({ x, y });
-    }
-  });
 
   useEffect(() => {
     const handleResize = () => {
       setStageWidth(window.innerWidth);
       setStageHeight(window.innerHeight);
-
     };
 
     window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
+
+  // * Generates the coordinates of the vertices
+  const vertices = VertexGrid({ polygonX, polygonY, vertexSpacing, rows });
 
   return (
     <Stage width={stageWidth} height={stageHeight}>
@@ -55,7 +41,7 @@ const GameBoard = () => {
         />
       </Layer>
       <Layer>
-      {vertices.map((pos, index) => (
+        {vertices.map((pos, index) => (
           <Vertex key={index} x={pos.x} y={pos.y} />
         ))}
       </Layer>
