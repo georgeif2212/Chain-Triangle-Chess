7f09a8a8a7480d035e996b2f8e7f17918b2047e6
georgeif2212 <jinfante2212@gmail.com>
2024-11-28 13:40:56 -0600

    fix: the imports were corrected!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import  Home  from "./pages/Home.jsx";
-import Game from "./pages/Game.jsx";
-import Error from "./pages/Error.jsx";
+import  Home  from "./modules/game/pages/Home.jsx";
+import Game from "./modules/game/pages/Game.jsx";
+import Error from "./modules/game/pages/Error.jsx";
 
 
 function App() {
