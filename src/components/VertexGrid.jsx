eaf18c21f5f33e78d38d412ce3dd893d4f70414c
georgeif2212 <jinfante2212@gmail.com>
2024-11-28 13:10:18 -0600

    feat: rowVertices array added to save the rows

--- src/components/VertexGrid.jsx
+++ src/components/VertexGrid.jsx
@@ -4,21 +4,21 @@ const VertexGrid = ({ polygonX, polygonY, vertexSpacing, rows }) => {
   const vertices = useMemo(() => {
     const calculatedVertices = [];
     rows.forEach((numVertices, rowIndex) => {
-      const yOffset =
-        polygonY + ((rowIndex - 3) * vertexSpacing * Math.sqrt(3)) / 2; // Ajuste en Y
-      const xOffsetStart = polygonX - ((numVertices - 1) * vertexSpacing) / 2; // Ajuste en X inicial
+      const yOffset = polygonY + ((rowIndex - 3) * vertexSpacing * Math.sqrt(3)) / 2;
+      const xOffsetStart = polygonX - ((numVertices - 1) * vertexSpacing) / 2;
 
+      const rowVertices = [];
       for (let i = 0; i < numVertices; i++) {
         const x = xOffsetStart + i * vertexSpacing;
         const y = yOffset;
-        calculatedVertices.push({ x, y });
+        rowVertices.push({ x, y, row: rowIndex, col: i });
       }
+      calculatedVertices.push(rowVertices); // Agrupa vértices en filas
     });
-    
     return calculatedVertices;
   }, [polygonX, polygonY, vertexSpacing, rows]);
-
-  return vertices;
+  console.log(vertices);
+  return vertices.flat(); // Aplanamos el arreglo para mantener la compatibilidad
 };
 
 export default VertexGrid;
