114d8686ea7eb0621b90ac6f806a59f10e610d03
georgeif2212 <jinfante2212@gmail.com>
2024-11-28 18:22:42 -0600

    feat: new routes to login, register and auth middleware added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import  Home  from "./modules/game/pages/Home.jsx";
+import Home from "./modules/game/pages/Home.jsx";
 import Game from "./modules/game/pages/Game.jsx";
 import Error from "./modules/game/pages/Error.jsx";
-
+import Register from "./modules/auth/pages/Register.jsx";
+import Login from "./modules/auth/pages/Login.jsx";
+import AuthMiddleware from "./middlewares/Authmiddleware.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <AuthMiddleware>
+              <Home />
+            </AuthMiddleware>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/game" element={<Game />} />
         <Route path="*" element={<Error />} />
       </Routes>
