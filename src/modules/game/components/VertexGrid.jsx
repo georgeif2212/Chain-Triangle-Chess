7d91e55362c73302f61094ade1d2e244dff40947
georgeif2212 <jinfante2212@gmail.com>
2024-12-17 12:38:57 -0600

    feat: vertex grid adapted and returning the index of the vertex

--- src/modules/game/components/VertexGrid.jsx
+++ src/modules/game/components/VertexGrid.jsx
@@ -1,24 +1,29 @@
-import { useMemo } from "react"; // * useMemo is useful, it calculates the vertices only when polygonX, polygonY, vertexSpacing, rows changes
+import { useMemo } from "react";
 
 const VertexGrid = ({ polygonX, polygonY, vertexSpacing, rows }) => {
   const vertices = useMemo(() => {
     const calculatedVertices = [];
+    let index = 0; // Contador para el índice de los vértices
+
+    const totalRows = rows.length;
+
     rows.forEach((numVertices, rowIndex) => {
-      const yOffset = polygonY + ((rowIndex - 3) * vertexSpacing * Math.sqrt(3)) / 2;
+      const yOffset =
+        polygonY +
+        ((rowIndex - (totalRows - 1) / 2) * vertexSpacing * Math.sqrt(3)) / 2;
       const xOffsetStart = polygonX - ((numVertices - 1) * vertexSpacing) / 2;
 
-      const rowVertices = [];
       for (let i = 0; i < numVertices; i++) {
         const x = xOffsetStart + i * vertexSpacing;
         const y = yOffset;
-        rowVertices.push({ x, y, row: rowIndex, col: i });
+        calculatedVertices.push({ x, y, index: index++ }); // Asigna un índice y lo incrementa
       }
-      calculatedVertices.push(rowVertices); // Agrupa vértices en filas
     });
+
     return calculatedVertices;
   }, [polygonX, polygonY, vertexSpacing, rows]);
-  console.log(vertices);
-  return vertices.flat(); // Aplanamos el arreglo para mantener la compatibilidad
+
+  return vertices;
 };
 
 export default VertexGrid;
