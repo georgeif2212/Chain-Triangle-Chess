363220ec35ed7da479a9f0c5dc79b874659ded16
georgeif2212 <jinfante2212@gmail.com>
2024-12-17 12:38:11 -0600

    feat: bootstrap container wrapping gameboard and title

--- src/modules/game/pages/Game.jsx
+++ src/modules/game/pages/Game.jsx
@@ -1,10 +1,14 @@
 import GameBoard from "../components/Gameboard.jsx";
+import Container from "react-bootstrap/Container";
+import "../styles/pages/Game.css"; 
 
 const Game = () => {
-  return <>
-    <h1>Bienvenido al juego</h1>
-    <GameBoard/>
-  </>;
+  return (
+    <Container className="game-container">
+      <h1>Bienvenido al juego</h1>
+      <GameBoard />
+    </Container>
+  );
 };
 
 export default Game;
