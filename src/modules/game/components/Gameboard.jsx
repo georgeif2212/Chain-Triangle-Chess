e9479c5193aa845a70c5ffd3effe729fc4998fdb
georgeif2212 <jinfante2212@gmail.com>
2025-01-12 20:21:55 -0600

    refactor: the code was refactored

--- src/modules/game/components/Gameboard.jsx
+++ src/modules/game/components/Gameboard.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Stage, Layer, RegularPolygon, Line } from "react-konva";
-import Vertex from "./Vertex.jsx";
+import { Stage, Layer, RegularPolygon } from "react-konva";
 import VertexGrid from "./VertexGrid.jsx";
-import Cord from "./Cord.jsx";
-
+import TriangleLayer from "./TriangleLayer.jsx";
+import ConnectionLayer from "./ConnectionLayer.jsx";
+import VertexLayer from "./VertexLayer.jsx";
 import { checkNewTriangles } from "./Rules.jsx";
 
 const GameBoard = () => {
@@ -20,7 +20,6 @@ const GameBoard = () => {
   const vertexSpacing = stageWidth / 10;
   const rows = [3, 4, 5, 4, 3];
 
-  // Generar coordenadas para los vértices
   const vertices = VertexGrid({ polygonX, polygonY, vertexSpacing, rows });
 
   useEffect(() => {
@@ -63,11 +62,8 @@ const GameBoard = () => {
 
       const generateNewTriangle = (coordinates) => {
         setTriangles((prevTriangles) => [...prevTriangles, coordinates]);
-        console.log("triangles: ", triangles);
-        console.log("COORDENADAS: ", coordinates);
       };
 
-      // Call function with onValidConnection Callback
       checkNewTriangles(
         vertex1,
         vertex2,
@@ -97,43 +93,9 @@ const GameBoard = () => {
             rotation={90}
           />
         </Layer>
-
-        <Layer>
-          {triangles.map((triangle, index) => (
-            <Line
-              key={index}
-              points={[
-                triangle[0].x,
-                triangle[0].y,
-                triangle[1].x,
-                triangle[1].y,
-                triangle[2].x,
-                triangle[2].y,
-              ]}
-              stroke="red"
-              strokeWidth={2}
-              fill="red"
-              closed
-            />
-          ))}
-        </Layer>
-
-        <Layer>
-          {connections.map((connection, index) => (
-            <Cord key={index} start={connection.start} end={connection.end} />
-          ))}
-        </Layer>
-
-        <Layer>
-          {vertices.map((pos, index) => (
-            <Vertex
-              key={index}
-              x={pos.x}
-              y={pos.y}
-              onClick={() => handleVertexClick(pos)}
-            />
-          ))}
-        </Layer>
+        <TriangleLayer triangles={triangles} />
+        <ConnectionLayer connections={connections} />
+        <VertexLayer vertices={vertices} onVertexClick={handleVertexClick} />
       </Stage>
     </div>
   );
