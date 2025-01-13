278430b32958651b5870c3775469daf9a42aeb5d
georgeif2212 <jinfante2212@gmail.com>
2025-01-12 19:55:57 -0600

    feat: first version of the algorithm with triangles working

--- src/modules/game/components/Rules.jsx
+++ src/modules/game/components/Rules.jsx
@@ -4,12 +4,18 @@ import { gameBoardMatrix } from "../../../utils/createArrays.jsx";
 import { sortArray } from "../../../utils/utils.js";
 import { triangles } from "../../../utils/utils.js";
 
-export function checkNewTriangles(vertex1, vertex2, onValidConnection) {
-  let p1_index = vertex1.index
-  let p2_index = vertex2.index
+export function checkNewTriangles(
+  vertex1,
+  vertex2,
+  onValidConnection,
+  vertices,
+  generateNewTriangle
+) {
+  let p1_index = vertex1.index;
+  let p2_index = vertex2.index;
 
   let intermediateEdge = matrixValidEdges[p1_index - 1][p2_index - 1];
-  
+
   if (intermediateEdge > -1) {
     return; // ! It means that is not a valid movement
   }
@@ -26,25 +32,37 @@ export function checkNewTriangles(vertex1, vertex2, onValidConnection) {
 
   // Check if triangle is formed
   for (let i = 0; i < newEdges.length; i++) {
-    const associatedVertices = matrixAssociatedVertices[newEdges[i][0] - 1][newEdges[i][1] - 1];
+    const associatedVertices =
+      matrixAssociatedVertices[newEdges[i][0] - 1][newEdges[i][1] - 1];
 
     for (let j = 0; j < associatedVertices.length; j++) {
-      const llaves_1 = sortArray(associatedVertices[j], newEdges[i][0]); 
+      const llaves_1 = sortArray(associatedVertices[j], newEdges[i][0]);
       const llaves_2 = sortArray(associatedVertices[j], newEdges[i][1]);
 
-      const wildcardEdge_1 = gameBoardMatrix[llaves_1[0] - 1][llaves_1[1] - 1]; 
-      const wildcardEdge_2 = gameBoardMatrix[llaves_2[0] - 1][llaves_2[1] - 1]; 
+      const wildcardEdge_1 = gameBoardMatrix[llaves_1[0] - 1][llaves_1[1] - 1];
+      const wildcardEdge_2 = gameBoardMatrix[llaves_2[0] - 1][llaves_2[1] - 1];
 
       if (wildcardEdge_1 === 1 && wildcardEdge_2 === 1) {
-        const verticesOfTriangle = sortArray([
-          associatedVertices[j],
-          ...newEdges[i],
-        ]);
-        const availableTriangle = triangles[verticesOfTriangle.toString()];
+        const verticesOfTriangle = [associatedVertices[j], ...newEdges[i]].sort(
+          (a, b) => a - b
+        );
 
-        if (availableTriangle !== 0) {
+        // console.log("VERTICESTRIANGLE: ", verticesOfTriangle);
+        // console.log("VERTICES: ", vertices);
+
+        const availableTriangle =
+          triangles[verticesOfTriangle.toString()];
+        console.log(availableTriangle);
+        if (availableTriangle === 0) {
+          const coordinates = verticesOfTriangle.map((vertexIndex) => {
+            const vertex = vertices.find((v) => v.index === vertexIndex);
+            return { x: vertex.x, y: vertex.y };
+          });
+          console.log("COORDENADAS: ",{...coordinates});
           triangles[verticesOfTriangle.toString()] = 1;
           console.log("SE FORMÓ TRIANGULO");
+          generateNewTriangle({...coordinates});
+
         }
       }
     }
