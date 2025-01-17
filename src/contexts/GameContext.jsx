cc0df1b1f550bdd206fdbb56fec96202bb6c3dc3
georgeif2212 <jinfante2212@gmail.com>
2025-01-16 18:15:31 -0600

    feat: new function to change to next team and the teams colors were added

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -2,29 +2,40 @@ import React, { createContext, useReducer } from "react";
 
 const GameContext = createContext();
 
+
 const initialState = {
-  mode: 'normal', // o 'questions'
+  mode: "normal",
   currentTeam: null,
-  teams: ['Team A', 'Team B', 'Team C', 'Team D'],
-  // Otros estados como conexiones, triángulos, etc.
-  gameStarted: false // Para saber si el juego ya comenzó
+  teams: [
+    { name: "Team A", color: "#EF4B4B" },
+    { name: "Team B", color: "#7BD3EA" },
+    { name: "Team C", color: "#A5DD9B" },
+    { name: "Team D", color: "#FFD966" }
+  ],
+  gameStarted: false,
 };
 
+const nextTeam = (state) => {
+  const currentIndex = state.teams.indexOf(state.currentTeam);
+  const nextIndex = (currentIndex + 1) % state.teams.length;
+  return state.teams[nextIndex];
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'SET_MODE':
+    case "SET_MODE":
       return { ...state, mode: action.payload };
-    case 'START_GAME':
+    case "START_GAME":
       return { ...state, gameStarted: true, currentTeam: state.teams[0] };
-    case 'SET_CURRENT_TEAM':
-      if (!state.gameStarted) return state; 
+    case "SET_CURRENT_TEAM":
+      if (!state.gameStarted) return state;
       return { ...state, currentTeam: action.payload };
+    case "NEXT_TEAM":
+      return { ...state, currentTeam: nextTeam(state) };
     default:
       return state;
   }
 };
-
 
 const GameProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
