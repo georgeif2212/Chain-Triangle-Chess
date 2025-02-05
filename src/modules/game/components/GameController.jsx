555ce8095ab5a89ca4f07e21cdfae175e3e5fb23
georgeif2212 <jinfante2212@gmail.com>
2025-02-04 20:55:14 -0500

    feat: button starts the game

--- src/modules/game/components/GameController.jsx
+++ src/modules/game/components/GameController.jsx
@@ -1,18 +1,33 @@
 import { useContext } from "react";
 import { GameContext } from "../../../contexts/GameContext.jsx";
 import GameBoard from "./Gameboard.jsx";
+import { Button } from "@mui/material";
 
-const   GameController = () => {
+const GameController = () => {
   const { state, dispatch } = useContext(GameContext);
   console.log(state);
+
   const handleNextTeam = () => {
     dispatch({ type: "NEXT_TEAM" });
   };
 
+  const handleStartGame = () => {
+    dispatch({ type: "START_GAME" });
+  };
+
   return (
     <>
-      <h2>Current Team: </h2>
-      <GameBoard currentTeam={state.currentTeam} onNextTeam={handleNextTeam} />
+      <h2>Current Team: {state.currentTeam ? state.currentTeam.name : "N/A"}</h2>
+      
+      {!state.gameStarted && (
+        <Button variant="contained" color="primary" onClick={handleStartGame}>
+          Comenzar juego
+        </Button>
+      )}
+
+      {state.gameStarted && (
+        <GameBoard currentTeam={state.currentTeam} onNextTeam={handleNextTeam} />
+      )}
     </>
   );
 };
