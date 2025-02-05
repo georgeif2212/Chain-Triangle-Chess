b9aa1e0cd1bb180f3fe9aabe9575038a724d0397
georgeif2212 <jinfante2212@gmail.com>
2025-02-04 21:24:14 -0500

    feat: modularized version of the gameboard

--- src/modules/game/components/Gameboard.jsx
+++ src/modules/game/components/Gameboard.jsx
@@ -1,121 +1,34 @@
-import React, { useEffect, useState, useRef, useContext } from "react";
-import { Stage, Layer, RegularPolygon } from "react-konva";
+import React, { useState } from "react";
+import { Stage } from "react-konva";
+import useStageSize from "../hooks/useStageSize";
+import useVertexSelection from "../hooks/useVertexSelection";
 import VertexGrid from "./VertexGrid.jsx";
 import TriangleLayer from "./TriangleLayer.jsx";
 import ConnectionLayer from "./ConnectionLayer.jsx";
 import VertexLayer from "./VertexLayer.jsx";
-import { checkNewTriangles } from "../services/Rules.jsx";
-import { GameContext } from "../../../contexts/GameContext.jsx";
+import HexagonLayer from "./HexagonLayer.jsx";
 
-const GameBoard = ({ currentTeam, onNextTeam }) => {
-  const { state, dispatch } = useContext(GameContext);
-
-  const [stageWidth, setStageWidth] = useState(0);
-  const [stageHeight, setStageHeight] = useState(0);
+const GameBoard = () => {
+  const { stageSize, containerRef } = useStageSize();
   const [connections, setConnections] = useState([]);
   const [triangles, setTriangles] = useState([]);
-  const [selectedVertex, setSelectedVertex] = useState(null);
-  const containerRef = useRef(null);
 
-  const polygonX = stageWidth / 2;
-  const polygonY = stageHeight / 2;
-  const radius = stageWidth / 4;
-  const vertexSpacing = stageWidth / 10;
+  const polygonX = stageSize.width / 2;
+  const polygonY = stageSize.height / 2;
+  const radius = stageSize.width / 4;
+  const vertexSpacing = stageSize.width / 10;
   const rows = [3, 4, 5, 4, 3];
 
   const vertices = VertexGrid({ polygonX, polygonY, vertexSpacing, rows });
-
-  useEffect(() => {
-    const currentContainer = containerRef.current;
-
-    const resizeObserver = new ResizeObserver((entries) => {
-      for (let entry of entries) {
-        setStageWidth(entry.contentRect.width);
-        setStageHeight(entry.contentRect.height);
-      }
-    });
-
-    if (currentContainer) {
-      resizeObserver.observe(currentContainer);
-    }
-
-    return () => {
-      if (currentContainer) {
-        resizeObserver.unobserve(currentContainer);
-      }
-    };
-  }, []);
-
-  const handleVertexClick = (vertex) => {
-    if (selectedVertex) {
-      const vertex1 =
-        selectedVertex.index < vertex.index ? selectedVertex : vertex;
-      const vertex2 =
-        selectedVertex.index > vertex.index ? selectedVertex : vertex;
-
-      const onValidConnection = (index1, index2) => {
-        setConnections((prevConnections) => [
-          ...prevConnections,
-          {
-            start: { ...selectedVertex, index: index1 },
-            end: { ...vertex, index: index2 },
-          },
-        ]);
-        dispatch({ type: "NEXT_TEAM" });
-      };
-
-      const generateNewTriangle = (coordinates) => {
-        const currentTeam = state.currentTeam;
-        
-        const newTriangle = {
-          coordinates,
-          team: currentTeam, 
-        };
-        
-        setTriangles((prevTriangles) => [...prevTriangles, newTriangle]);
-      };
-
-      checkNewTriangles(
-        vertex1,
-        vertex2,
-        onValidConnection,
-        vertices,
-        generateNewTriangle
-      );
-
-      setSelectedVertex(null);
-    } else {
-      setSelectedVertex(vertex);
-    }
-  };
+  const handleVertexClick = useVertexSelection(vertices, setConnections, setTriangles);
 
   return (
     <div ref={containerRef} style={{ width: "100%", height: "100%" }}>
-      <Stage width={stageWidth} height={stageHeight}>
-        <Layer>
-          <RegularPolygon
-            x={polygonX}
-            y={polygonY}
-            sides={6}
-            radius={radius}
-            fill="#EAD8C0"
-            stroke="#A79277"
-            strokeWidth={4}
-            rotation={90}
-          />
-        </Layer>
-
-        <Layer>
-          <TriangleLayer triangles={triangles} />
-        </Layer>
-
-        <Layer>
-          <ConnectionLayer connections={connections} />
-        </Layer>
-
-        <Layer>
-          <VertexLayer vertices={vertices} onVertexClick={handleVertexClick} />
-        </Layer>
+      <Stage width={stageSize.width} height={stageSize.height}>
+        <HexagonLayer x={polygonX} y={polygonY} radius={radius} />
+        <TriangleLayer triangles={triangles} />
+        <ConnectionLayer connections={connections} />
+        <VertexLayer vertices={vertices} onVertexClick={handleVertexClick} />
       </Stage>
     </div>
   );
