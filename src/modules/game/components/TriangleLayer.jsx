06050c80a4695be329f056e5063300f7b4ead588
georgeif2212 <jinfante2212@gmail.com>
2025-02-04 20:19:24 -0500

    fix: triangle coordinates adapted to game context provider

--- src/modules/game/components/TriangleLayer.jsx
+++ src/modules/game/components/TriangleLayer.jsx
@@ -1,22 +1,27 @@
 import { Line } from "react-konva";
 
-const TriangleLayer = ({ triangles }) => (
-  <>
-    {triangles.map((triangle, index) => (
-      <Line
-        key={index}
-        points={[
-          triangle[0].x, triangle[0].y,
-          triangle[1].x, triangle[1].y,
-          triangle[2].x, triangle[2].y,
-        ]}
-        stroke="#EF4B4B"
-        strokeWidth={2}
-        fill="#EF4B4B"
-        closed
-      />
-    ))}
-  </>
-);
+  const TriangleLayer = ({ triangles }) => {
 
+  return (
+    <>
+      {triangles.map((triangle, index) => (
+        <Line
+          key={index}
+          points={[
+            triangle.coordinates[0].x,
+            triangle.coordinates[0].y,
+            triangle.coordinates[1].x,
+            triangle.coordinates[1].y,
+            triangle.coordinates[2].x,
+            triangle.coordinates[2].y,
+          ]}
+          stroke="#EF4B4B"
+          strokeWidth={2}
+          fill="#EF4B4B"
+          closed
+        />
+      ))}
+    </>
+  );
+};
 export default TriangleLayer;
