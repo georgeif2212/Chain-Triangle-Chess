887aa1992289954e38ee47008014d6705f7a7c5f
georgeif2212 <jinfante2212@gmail.com>
2025-02-11 21:52:57 -0500

    files renamed to jsx files

--- src/index.jsx
+++ src/index.jsx
@@ -1,44 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence,
-} from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import {
+//   getAuth,
+//   setPersistence,
+//   browserLocalPersistence,
+// } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
 
-import { AuthProvider } from "./contexts/AuthContext.jsx";
+// import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { GameProvider } from "./contexts/GameContext.jsx";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECTID,
+//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+//   appId: process.env.REACT_APP_APPID,
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
 
-setPersistence(auth, browserLocalPersistence);
+//  setPersistence(auth, browserLocalPersistence);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
+    <>
       <GameProvider>
         <App />
       </GameProvider>
-    </AuthProvider>
+    </>
   </React.StrictMode>
 );
 
