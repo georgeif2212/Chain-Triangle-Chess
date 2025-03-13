28f238756f7c2a482ac85f92b80f47e1c689488a
Jorge Infante <jinfante2212@gmail.com>
2025-03-13 14:48:10 -0500

    fix: game page is going to replace GameController.jsx

--- src/modules/game/pages/Game.jsx
+++ src/modules/game/pages/Game.jsx
@@ -1,13 +1,32 @@
-import GameController from "../components/GameController.jsx";
-import "../styles/pages/Game.css"; 
+import { useContext } from "react";
+import { Typography, Button } from "@mui/material";
+import { GameContext } from "../../../contexts/GameContext.jsx";
+import GameBoard from "../components/Gameboard.jsx";
+import "../styles/pages/Game.css";
 
 const Game = () => {
+  const { state, dispatch } = useContext(GameContext);
+
+  const handleStartGame = () => {
+    dispatch({ type: "START_GAME" });
+  };
   return (
     <div className="game-container">
-      <h1>Bienvenido al juego</h1>
-      <GameController />
+      <Typography variant="h3">Triangle Chess!</Typography>
+      {!state.gameStarted ? (
+        <Button variant="contained" color="primary" onClick={handleStartGame}>
+          Comenzar juego
+        </Button>
+      ) : (
+        <>
+          <Typography variant="h5">
+            Turno de: <strong>{state.currentTeam.name}</strong>
+          </Typography>
+          <GameBoard />
+        </>
+      )}
     </div>
   );
-};  
+};
 
 export default Game;
