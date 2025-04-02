3a3ee437b39b7080812aed669d86225f74120e45
Jorge Infante <jinfante2212@gmail.com>
2025-04-02 10:29:54 -0500

    feat: home page was implemented and it redirects to game or options

--- src/modules/game/pages/Home.jsx
+++ src/modules/game/pages/Home.jsx
@@ -1,8 +1,15 @@
-const Menu =()=>{
-  return <>
-  <h1>Jugar</h1>
-  <h2>Opciones</h2>
-  <h2>Salir</h2>
-  </>
-  
-}
+import { Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h3">Triangle Chess </Typography>
+      <Link to="/game"><h1>Jugar</h1></Link>
+      <Link to="/game"><h2>Opciones</h2></Link>      
+      <h2>Salir</h2>
+    </Container>
+  );
+};
+
+export default Home;
