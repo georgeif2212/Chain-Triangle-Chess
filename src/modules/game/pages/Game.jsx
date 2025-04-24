5a7b008755a0ad4fa46ef3e30bab1b450096831a
Jorge Infante <jinfante2212@gmail.com>
2025-04-24 13:22:10 -0500

    feat: the function to render the content when game has not started has been modularized into a new component

--- src/modules/game/pages/Game.jsx
+++ src/modules/game/pages/Game.jsx
@@ -1,55 +1,14 @@
-import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
-import { Typography, Button, Container } from "@mui/material";
+import { Typography, Container } from "@mui/material";
 import { GameContext } from "../../../contexts/GameContext.jsx";
 import GameBoard from "../components/Gameboard.jsx";
 import GameHasFinished from "./GameHasFinished.jsx";
+import GameHasNotStarted from "./GameHasNotStarted.jsx";
 import "../styles/pages/Game.css";
 
 const Game = () => {
-  const { state, dispatch } = useContext(GameContext);
-  const handleStartGame = () => {
-    const updatedTeams = state.teams.map((team) => ({
-      ...team,
-      score: 0,
-    }));
-
-    dispatch({ type: "SET_TEAMS", payload: updatedTeams });
-    dispatch({ type: "START_GAME" });
-  };
-
-  const renderGameNotStarted = () => {
-    const navigate = useNavigate();
-    return (
-      <>
-        <div>
-          <p>
-            <strong>Mode: </strong>
-            {state.mode}
-          </p>
-          <p>Equipos: </p>
-          {state.teams.map((team, index) => (
-            <p key={index}>
-              <strong>Team {index + 1}: </strong>
-              {team.name}
-            </p>
-          ))}
-        </div>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={() => navigate("/options")}
-        >
-          Configurar
-        </Button>
-        <Button variant="contained" color="primary" onClick={handleStartGame}>
-          Comenzar juego
-        </Button>
-      </>
-    );
-  };
-
-  const renderGameContent = () => (
+  const { state } = useContext(GameContext);
+  const GameContent = () => (
     <>
       <div>
         {state.teams.map((team, index) => (
@@ -70,8 +29,8 @@ const Game = () => {
   return (
     <Container className="game-container">
       <Typography variant="h3">Triangle Chess!</Typography>
-      {state.gameState === "notStarted" && renderGameNotStarted()}
-      {state.gameState === "started" && renderGameContent()}
+      {state.gameState === "notStarted" && GameHasNotStarted()}
+      {state.gameState === "started" && GameContent()}
       {state.gameState === "finished" && GameHasFinished(state)}
     </Container>
   );
