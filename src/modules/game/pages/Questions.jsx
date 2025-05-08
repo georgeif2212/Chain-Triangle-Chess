e772c7cb9cca9d5ba306c23804a47c24fd7ec11e
Jorge Infante <jinfante2212@gmail.com>
2025-05-07 19:58:47 -0500

    temporal: page for extracting questions from the database

---
+++ src/modules/game/pages/Questions.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/material";
+
+const Questions = () => {
+  return (
+    <Container>
+      <h1>Hola prueba</h1>
+    </Container>  
+  );
+};
+
+export default Questions;
