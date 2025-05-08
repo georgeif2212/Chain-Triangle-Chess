19764f7a23c9647ad1f70d7c9699e549525603ac
Jorge Infante <jinfante2212@gmail.com>
2025-05-07 19:58:15 -0500

    feat: configuration of vaep firebase implemented

---
+++ src/firebase/firebaseQuestionsDB.js
@@ -0,0 +1,30 @@
+// firebaseConfig.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const vaepFirebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_VAEP_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_APP_VAEP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_VAEP_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_APP_VAEP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_VAEP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_VAEP_APPID,
+  measurementId: import.meta.env.VITE_REACT_APP_VAEP_MEASUREMENTID,
+};
+
+let secondaryApp;
+
+const existingApps = getApps();
+const alreadyInitialized = existingApps.find(app => app.name === "secondary");
+
+if (!alreadyInitialized) {
+  secondaryApp = initializeApp(vaepFirebaseConfig, "secondary"); // nombre personalizado
+} else {
+  secondaryApp = alreadyInitialized;
+}
+
+export default secondaryApp;
