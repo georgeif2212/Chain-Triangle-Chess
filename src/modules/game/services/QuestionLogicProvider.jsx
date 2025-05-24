4197e15cf36ad89376ec61f55a13776d7d1be3c5
Jorge Infante <jinfante2212@gmail.com>
2025-05-24 11:47:21 -0500

    feat: new service in charge of set the questions in context

---
+++ src/modules/game/services/QuestionLogicProvider.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useQuestions } from "../hooks/useQuestions.jsx";
+import { useContext } from "react";
+import { GameContext } from "../../../contexts/GameContext.jsx"; // o donde guardes tus preguntas
+
+const QuestionLogicProvider = ({ materia, tema, token }) => {
+  const {
+    preguntas,
+    respuestas,
+    opciones,
+    materiaNombre,
+    temaNombre,
+    errorSesion,
+  } = useQuestions({ materia, tema, token });
+  const { dispatch } = useContext(GameContext);
+
+  useEffect(() => {
+    if (preguntas.length && respuestas.length && opciones.length) {
+      dispatch({
+        type: "LOAD_QUESTIONS",
+        payload: {
+          preguntas,
+          respuestas,
+          opciones,
+          materiaNombre,
+          temaNombre,
+        },
+      });
+    }
+    if (errorSesion) {
+      console.error("Sesión expirada. Redirigiendo...");
+      setTimeout(() => {
+        window.location.href = "https://vaep-uamc.web.app?sesionExpirada=true";
+      }, 3000);
+    }
+  }, [preguntas, respuestas, opciones, dispatch, materiaNombre, temaNombre, errorSesion]);
+
+  return null; // no renderiza nada
+};
+
+export default QuestionLogicProvider;
