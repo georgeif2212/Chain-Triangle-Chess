a8c7014269072b111d97414266989efbb27b162d
Jorge Infante <jinfante2212@gmail.com>
2025-06-15 22:13:10 -0500

    fix: imports resolved

--- src/modules/game/components/QuestionDialog.jsx
+++ src/modules/game/components/QuestionDialog.jsx
@@ -4,6 +4,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { useState } from "react";
 
 const QuestionDialog = ({
   open,
