413dae029fbc72f08cdd20f22ba0fbfedac41e16
Jorge Infante <jinfante2212@gmail.com>
2025-06-17 15:46:56 -0500

    feat: strategy which ables the question mode)

---
+++ src/modules/game/services/strategies/QuestionMoveStrategy.js
@@ -0,0 +1,75 @@
+import {
+  isValidConnection,
+  registerNewConnection,
+  checkAndRegisterTriangle,
+} from "../Rules.jsx";
+import { matrixValidEdges } from "../../../../utils/createArrays.jsx";
+
+
+class QuestionMoveStrategy {
+  constructor({
+    vertex1,
+    vertex2,
+    onValidConnection,
+    vertices,
+    generateNewTriangle,
+    context,
+    onInvalidConnection,
+    showQuestion,
+  }) {
+    this.vertex1 = vertex1;
+    this.vertex2 = vertex2;
+    this.onValidConnection = onValidConnection;
+    this.vertices = vertices;
+    this.generateNewTriangle = generateNewTriangle;
+    this.context = context;
+    this.onInvalidConnection = onInvalidConnection;
+    this.showQuestion = showQuestion;
+  }
+
+  execute() {
+    if (!isValidConnection(this.vertex1, this.vertex2)) {
+      this.onInvalidConnection();
+      return;
+    }
+
+    const { state, dispatch } = this.context;
+
+    const preguntaIdx = Math.floor(
+      Math.random() * state.vaepData.preguntas.length
+    );
+    const pregunta = state.vaepData.preguntas[preguntaIdx];
+    const opciones = state.vaepData.opciones[preguntaIdx];
+    const respuestaCorrecta = state.vaepData.respuestas[preguntaIdx];
+
+    this.showQuestion({
+      question: pregunta,
+      options: opciones,
+      correctAnswer: respuestaCorrecta,
+      onSuccess: () => {
+        this.onValidConnection(this.vertex1.index, this.vertex2.index);
+        const intermediateEdge = registerNewConnection(
+          this.vertex1,
+          this.vertex2,
+          matrixValidEdges[this.vertex1.index - 1][this.vertex2.index - 1]
+        );
+        const newEdges = [
+          [this.vertex1.index, intermediateEdge],
+          [intermediateEdge, this.vertex2.index],
+        ];
+        checkAndRegisterTriangle(
+          newEdges,
+          this.vertices,
+          this.generateNewTriangle,
+          this.context
+        );
+        dispatch({ type: "NEXT_TEAM" });
+      },
+      onFail: () => {
+        dispatch({ type: "NEXT_TEAM" });
+      },
+    });
+  }
+}
+
+export default QuestionMoveStrategy;
