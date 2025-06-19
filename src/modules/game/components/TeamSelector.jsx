bf6555527047047847f8ed039db02a0b224194fd
Jorge Infante <jinfante2212@gmail.com>
2025-06-18 20:21:40 -0500

    feat: new design to show the team selector

--- src/modules/game/components/TeamSelector.jsx
+++ src/modules/game/components/TeamSelector.jsx
@@ -1,27 +1,40 @@
-import { TextField, MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+import {
+  TextField,
+  MenuItem,
+  Select,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
 import { availableColors } from "../../../utils/utils";
+import styles from "../styles/components/TeamSelector.module.css";
 
 const TeamSelector = ({ index, team, teams, onTeamChange }) => {
   return (
-    <div className="team-selector">
+    <div className={styles.teamSelector}>
       <TextField
         label={`Equipo ${index + 1}`}
         value={team.name}
         onChange={(e) => onTeamChange(index, "name", e.target.value)}
         variant="outlined"
         size="small"
+        className={styles.teamInput}
       />
-      <FormControl size="small">
+      <FormControl size="small" className={styles.colorSelectControl}>
         <InputLabel>Color</InputLabel>
         <Select
           value={team.color}
-          onChange={(e) => onTeamChange(index, "color", e.target.value)}>
+          onChange={(e) => onTeamChange(index, "color", e.target.value)}
+        >
           {availableColors.map((color) => (
             <MenuItem
               key={color}
               value={color}
-              disabled={teams.some((t) => t.color === color)}>
-              <div className="color-box" style={{ backgroundColor: color }}></div>
+              disabled={teams.some((t) => t.color === color)}
+            >
+              <div
+                className={styles.colorBox}
+                style={{ backgroundColor: color }}
+              ></div>
             </MenuItem>
           ))}
         </Select>
