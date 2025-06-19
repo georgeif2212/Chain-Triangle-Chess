54f5e7bca921925ab7ef032424693d440f8971d3
Jorge Infante <jinfante2212@gmail.com>
2025-06-19 12:12:37 -0500

    feat: new animation to connect vertices

--- src/modules/game/components/Cord.jsx
+++ src/modules/game/components/Cord.jsx
@@ -1,14 +1,29 @@
-// Cord.jsx
 import { Line } from "react-konva";
+import { useRef, useEffect, useState } from "react";
 
-const Cord = ({ start, end }) => {
+const Cord = ({ start, end, fromIndex }) => {
+  const lineRef = useRef(null);
+  const [drawn, setDrawn] = useState(false);
+
+  const from = fromIndex === start.index ? start : end;
+  const to = fromIndex === start.index ? end : start;
+
+  useEffect(() => {
+    if (lineRef.current && !drawn) {
+      lineRef.current.points([from.x, from.y, from.x, from.y]);
+
+      lineRef.current.to({
+        points: [from.x, from.y, to.x, to.y],
+        duration: 0.2,
+        easing: Konva.Easings.EaseInOut,
+      });
+
+      setDrawn(true);
+    }
+  }, [from, to, drawn]);
+
   return (
-    <Line
-      points={[start.x, start.y, end.x, end.y]} 
-      stroke="#131010" 
-      strokeWidth={4} 
-      lineCap="round"
-    />
+    <Line ref={lineRef} stroke="#5c4936" strokeWidth={4} lineCap="round" />
   );
 };
 
