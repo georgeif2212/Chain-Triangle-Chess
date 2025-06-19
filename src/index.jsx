93fb0ea80eda92bdc455ad234d51d4d7f3c9dc6c
Jorge Infante <jinfante2212@gmail.com>
2025-06-19 18:30:01 -0500

    fix: remove useless comments

--- src/index.jsx
+++ src/index.jsx
@@ -2,22 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import reportWebVitals from "./reportWebVitals";
-import { analytics } from "./firebase/firebaseConfiguration.js";
-// import {
-//   getAuth,
-//   setPersistence,
-//   browserLocalPersistence,
-// } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-
-// import { AuthProvider } from "./contexts/AuthContext.jsx";
 import { GameProvider } from "./contexts/GameContext.jsx";
 
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-
-//  setPersistence(auth, browserLocalPersistence);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -30,7 +16,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
