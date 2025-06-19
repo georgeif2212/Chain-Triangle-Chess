288a1810016ca18cf9ad4480efc9f60dae6ac7c7
Jorge Infante <jinfante2212@gmail.com>
2025-06-19 11:57:16 -0500

    feat: new animation to color the triangles

--- src/modules/game/components/TriangleLayer.jsx
+++ src/modules/game/components/TriangleLayer.jsx
@@ -1,26 +1,48 @@
-import { Line,Layer } from "react-konva";
+import { Line, Layer } from "react-konva";
+import { useEffect, useRef } from "react";
 
-  const TriangleLayer = ({ triangles }) => {
+const Triangle = ({ triangle }) => {
+  const triangleRef = useRef(null);
+
+  useEffect(() => {
+    if (triangleRef.current) {
+      triangleRef.current.to({
+        fill: triangle.team.color,
+        opacity: 1,
+        duration: 0.5,
+        easing: Konva.Easings.EaseInOut,
+      });
+    }
+  }, [triangle.team.color]);
+
+  return (
+    <Line
+      ref={triangleRef}
+      points={[
+        triangle.coordinates[0].x,
+        triangle.coordinates[0].y,
+        triangle.coordinates[1].x,
+        triangle.coordinates[1].y,
+        triangle.coordinates[2].x,
+        triangle.coordinates[2].y,
+      ]}
+      stroke="#5a5a5a"
+      strokeWidth={1}
+      fill={triangle.team.color}
+      opacity={0} 
+      closed
+    />
+  );
+};
+
+const TriangleLayer = ({ triangles }) => {
   return (
     <Layer>
       {triangles.map((triangle, index) => (
-        <Line
-          key={index}
-          points={[
-            triangle.coordinates[0].x,
-            triangle.coordinates[0].y,
-            triangle.coordinates[1].x,
-            triangle.coordinates[1].y,
-            triangle.coordinates[2].x,
-            triangle.coordinates[2].y,
-          ]}
-          stroke={triangle.team.color}
-          strokeWidth={2}
-          fill={triangle.team.color}
-          closed
-        />
+        <Triangle key={index} triangle={triangle} />
       ))}
     </Layer>
   );
 };
+
 export default TriangleLayer;
