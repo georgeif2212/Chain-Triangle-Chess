a0be4d2d8c2969648380356e24125789092d2294
Jorge Infante <jinfante2212@gmail.com>
2025-06-19 11:46:57 -0500

    feat: new colors for the gameboard

--- src/modules/game/components/HexagonLayer.jsx
+++ src/modules/game/components/HexagonLayer.jsx
@@ -7,10 +7,17 @@ const HexagonLayer = ({ x, y, radius }) => (
       y={y}
       sides={6}
       radius={radius}
-      fill="#EAD8C0"
-      stroke="#A79277"
-      strokeWidth={4}
+      fillLinearGradientStartPoint={{ x: -radius, y: -radius }}
+      fillLinearGradientEndPoint={{ x: radius, y: radius }}
+      fillLinearGradientColorStops={[0, "#F5E9D5", 1, "#EAD8C0"]} 
+      stroke="#8B6F4E" 
+      strokeWidth={5}
       rotation={90}
+      shadowColor="#A79277"
+      shadowBlur={20}
+      shadowOffset={{ x: 5, y: 5 }}
+      shadowOpacity={0.25}
+      perfectDrawEnabled={false}
     />
   </Layer>
 );
