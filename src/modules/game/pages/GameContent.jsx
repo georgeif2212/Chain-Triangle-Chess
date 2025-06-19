84732f5b7361f80e7586ca9a38f2d238dccea6da
Jorge Infante <jinfante2212@gmail.com>
2025-06-18 19:21:20 -0500

    feat: new styles

--- src/modules/game/pages/GameContent.jsx
+++ src/modules/game/pages/GameContent.jsx
@@ -4,7 +4,7 @@ import { GameContext } from "../../../contexts/GameContext.jsx";
 import GameBoard from "../components/Gameboard.jsx";
 import { useGameParams } from "../hooks/useGameParams.jsx";
 import { useLoadQuestions } from "../hooks/useLoadQuestions.jsx";
-import { darkenColor } from "../../../utils/utils.js";
+import { darkenHexColor } from "../../../utils/utils.js";
 import styles from "../styles/components/GameContent.module.css";
 import { formatMode } from "../../../utils/utils.js";
 
@@ -22,32 +22,38 @@ const GameContent = () => {
 
   return (
     <div className={styles.wrapper}>
-      <Typography variant="h3">Triangle Chess!</Typography>
-      <div className={styles.statusInfo}>
-        <Typography variant="h6">
-          Modo: <strong>{formatMode(state.mode)}</strong>
-        </Typography>
+      <div className={styles.sidebar}>
+        <h1>Triangle Chess!</h1>
+
+        <div className={styles.statusInfo}>
+          <Typography variant="h6">
+            Turno de: <strong>{state.currentTeam?.name}</strong>
+          </Typography>
+          <Typography variant="subtitle1">
+            Modo: <strong>{formatMode(state.mode)}</strong>
+          </Typography>
+        </div>
+        
+        <div className={styles.topBar}>
+          {state.teams.map((team, index) => (
+            <div
+              key={index}
+              className={styles.teamBox}
+              style={{
+                backgroundColor: team.color,
+                borderColor: darkenHexColor(team.color, 0.3),
+              }}
+            >
+              <Typography variant="h6" className={styles.teamText}>
+                {team.name}: {team.score} pts
+              </Typography>
+            </div>
+          ))}
+        </div>
+
+        
       </div>
 
-      <div className={styles.topBar}>
-        {state.teams.map((team, index) => (
-          <div
-            key={index}
-            className={styles.teamBox}
-            style={{
-              backgroundColor: team.color,
-              border: `3px solid ${darkenColor(team.color)}`,
-            }}
-          >
-            <Typography variant="h6" className={styles.teamText}>
-              {team.name}: {team.score} pts
-            </Typography>
-          </div>
-        ))}
-      </div>
-      <Typography variant="subtitle1">
-        Turno de: <strong>{state.currentTeam?.name}</strong>
-      </Typography>
       <GameBoard />
     </div>
   );
