6a0dfde4fef90961f2c9c2d2e2644a2aa212e7bd
Jorge Infante <jinfante2212@gmail.com>
2025-06-18 20:01:34 -0500

    feat: new design to show main menu

--- src/modules/game/pages/GameHasNotStarted.jsx
+++ src/modules/game/pages/GameHasNotStarted.jsx
@@ -1,12 +1,15 @@
+// GameHasNotStarted.jsx
 import { useNavigate } from "react-router-dom";
-import { GameContext } from "../../../contexts/GameContext.jsx";
+import { GameContext } from "../../../contexts/GameContext";
 import { useContext } from "react";
-import { Button } from "@mui/material";
-import { formatMode } from "../../../utils/utils.js";
+import { Button, Typography, Box } from "@mui/material";
+import { formatMode } from "../../../utils/utils";
+import styles from "../styles/pages/GameHasNotStarted.module.css";
 
 const GameHasNotStarted = () => {
-  const {state, dispatch}=useContext(GameContext)
+  const { state, dispatch } = useContext(GameContext);
   const navigate = useNavigate();
+  console.log(state);
 
   const handleStartGame = () => {
     const updatedTeams = state.teams.map((team) => ({
@@ -19,31 +22,42 @@ const GameHasNotStarted = () => {
   };
 
   return (
-    <>
-      <div>
-        <p>
-          <strong>Mode: </strong>
-          {formatMode(state.mode)}
-        </p>
-        <p>Equipos: </p>
+    <div className={styles.wrapper}>
+      <Typography variant="h3" className={styles.title}>
+        Triangle Chess!
+      </Typography>
+
+      <Typography variant="h6" className={styles.mode}>
+        Modo: <strong>{formatMode(state.mode)}</strong>
+      </Typography>
+
+      <div className={styles.teamsContainer}>
         {state.teams.map((team, index) => (
-          <p key={index}>
-            <strong>Team {index + 1}: </strong>
-            {team.name}
-          </p>
+          <div
+            key={index}
+            className={styles.teamCard}
+            style={{ borderColor: team.color }}
+          >
+            <Typography variant="body1" className={styles.teamText}>
+              Equipo {index + 1}: <strong>{team.name}</strong>
+            </Typography>
+          </div>
         ))}
       </div>
-      <Button
-        variant="contained"
-        color="secondary"
-        onClick={() => navigate(`/options${location.search}`)}
-      >
-        Configurar
-      </Button>
-      <Button variant="contained" color="primary" onClick={handleStartGame}>
-        Comenzar juego
-      </Button>
-    </>
+
+      <div className={styles.buttonsWrapper}>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => navigate(`/options${location.search}`)}
+        >
+          Configurar
+        </Button>
+        <Button variant="contained" color="primary" onClick={handleStartGame}>
+          Comenzar juego
+        </Button>
+      </div>
+    </div>
   );
 };
 
