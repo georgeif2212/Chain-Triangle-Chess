17534539badb724386331c86f390ec47b99de45b
Jorge Infante <jinfante2212@gmail.com>
2025-06-19 10:47:11 -0500

    feat: pass the vertexIndex as prop!

--- src/modules/game/components/VertexLayer.jsx
+++ src/modules/game/components/VertexLayer.jsx
@@ -1,12 +1,13 @@
 import Vertex from "./Vertex.jsx";
 import { Layer } from "react-konva";
-const VertexLayer = ({ vertices, onVertexClick }) => (
+const VertexLayer = ({ vertices, onVertexClick, selectedVertex }) => (
   <Layer>
     {vertices.map((pos, index) => (
       <Vertex
         key={index}
         x={pos.x}
         y={pos.y}
+        isSelected={index === selectedVertex - 1}
         onClick={() => onVertexClick(pos)}
       />
     ))}
