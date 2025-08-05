2f3a9d6e8fc9453f64816dc25978c7d1ae2dbe5a
Jorge Infante <jinfante2212@gmail.com>
2025-08-04 20:19:22 -0500

    feat: new alias for each route

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()]
-})
+  base: "/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/game/components"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@hooks": path.resolve(__dirname, "src/game/hooks"),
+      "@pages": path.resolve(__dirname, "src/game/pages"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@styles": path.resolve(__dirname, "src/game/styles"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@customFirebase": path.resolve(__dirname, "src/firebase"),
+      "@services": path.resolve(__dirname, "src/game/services"),
+    },
+  },
+});
