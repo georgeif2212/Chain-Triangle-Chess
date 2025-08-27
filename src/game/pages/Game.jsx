1058391d72242e2a7da71929df7a01229c11941a
Jorge Infante <jinfante2212@gmail.com>
2025-08-27 16:18:53 -0500

    fix: user, subject are not received anymore

--- src/game/pages/Game.jsx
+++ src/game/pages/Game.jsx
@@ -2,26 +2,30 @@ import { useContext } from "react";
 import { Container } from "@mui/material";
 import GameContent from "./GameContent.jsx";
 import GameHeader from "@components/layout/GameHeader.jsx";
-import Footer from "@components/layout/Footer.jsx"
+import Footer from "@components/layout/Footer.jsx";
 import { GameContext } from "@contexts/GameContext.jsx";
 import "@styles/pages/Game.css";
 
 const Game = () => {
   const { state } = useContext(GameContext);
-  console.log("STATE:", state.vaepData);
+
+  const usuario = state.vaepData?.user?.email || "Sin usuario";
+  const materia = state.vaepData?.materiaNombre || "Sin materia";
+  const tema = state.vaepData?.temaNombre || "Sin tema";
+
   return (
     <>
       <GameHeader
-        usuario={state.vaepData?.user.email ?? "Sin usuario"}
-        materia={state.vaepData?.materiaNombre ?? "Sin materia"}
-        tema={state.vaepData?.temaNombre ?? "Sin tema"}
+        usuario={usuario}
+        materia={materia}
+        tema={tema}
         onBack={() => navigate("/dashboard")}
       />
-
+      ;
       <Container className="game-container">
         <GameContent />
       </Container>
-      <Footer/>
+      <Footer />
     </>
   );
 };
