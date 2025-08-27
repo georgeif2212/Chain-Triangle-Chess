58f33f00139ae5c1e1d804f509c3d06348b248ba
Jorge Infante <jinfante2212@gmail.com>
2025-08-27 16:44:36 -0500

    fix: the header in footer were removed from this page

--- src/game/pages/Game.jsx
+++ src/game/pages/Game.jsx
@@ -1,32 +1,11 @@
-import { useContext } from "react";
 import { Container } from "@mui/material";
 import GameContent from "./GameContent.jsx";
-import GameHeader from "@components/layout/GameHeader.jsx";
-import Footer from "@components/layout/Footer.jsx";
-import { GameContext } from "@contexts/GameContext.jsx";
 import "@styles/pages/Game.css";
-
 const Game = () => {
-  const { state } = useContext(GameContext);
-
-  const usuario = state.vaepData?.user?.email || "Sin usuario";
-  const materia = state.vaepData?.materiaNombre || "Sin materia";
-  const tema = state.vaepData?.temaNombre || "Sin tema";
-
   return (
-    <>
-      <GameHeader
-        usuario={usuario}
-        materia={materia}
-        tema={tema}
-        onBack={() => navigate("/dashboard")}
-      />
-      ;
-      <Container className="game-container">
-        <GameContent />
-      </Container>
-      <Footer />
-    </>
+    <Container className="game-container">
+      <GameContent />
+    </Container>
   );
 };
 
