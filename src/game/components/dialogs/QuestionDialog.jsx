fdb074922b0d92be7cf6967769408c72e15c6e86
Jorge Infante <jinfante2212@gmail.com>
2025-08-28 23:38:25 -0500

    feat: new styles

--- src/game/components/dialogs/QuestionDialog.jsx
+++ src/game/components/dialogs/QuestionDialog.jsx
@@ -1,10 +1,11 @@
+import { useState, useEffect } from "react";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { useState, useEffect } from "react";
+import styles from "@styles/components/dialogs/QuestionDialog.module.css";
 
 const QuestionDialog = ({
   open,
@@ -29,45 +30,80 @@ const QuestionDialog = ({
     }
   };
 
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+
   return (
     <Dialog
       open={open}
       onClose={(event, reason) => {
-        if (reason === "backdropClick" || reason === "escapeKeyDown") {
-          return;
-        }
+        if (reason === "backdropClick" || reason === "escapeKeyDown") return;
         onClose();
       }}
+      PaperProps={{
+        sx: {
+          backgroundColor: "#f9f5f2de",
+          borderRadius: 2,
+          minWidth: 300,
+        },
+      }}
     >
-      <DialogTitle>Pregunta</DialogTitle>
+      <DialogTitle textAlign="center">{question}</DialogTitle>
       <DialogContent>
-        <Typography>{question}</Typography>
-
         {options?.length > 0 ? (
-          // Pregunta de opción múltiple
-          options.map((opt, i) => (
-            <Button key={i} onClick={() => handleOptionClick(opt)}>
-              {opt}
-            </Button>
-          ))
+          <div className={styles.optionsColumn}>
+            {options.map((opt, i) => (
+              <Button
+                key={i}
+                fullWidth
+                variant="outlined"
+                onClick={() => handleOptionClick(opt)}
+                sx={{
+                  color: "#412305ff",
+                  justifyContent: "flex-start",
+                  marginBottom: 1,
+                  textTransform: "none",
+                  backgroundColor: "#e0dcd8",
+                  borderColor: "#a18f7bff",
+                  "&:hover": {
+                    backgroundColor: "#d4b9a6ff",
+                  },
+                }}
+              >
+                {letters[i]}) {opt}
+              </Button>
+            ))}
+          </div>
         ) : (
-          // Pregunta abierta
-          <>
-            <Button onClick={() => setShowAnswer(true)}>
-              Mostrar respuesta
-            </Button>
+          <div className={styles.responsesBox}>
+            <div className={styles.extra}>
+              {!showAnswer && (
+                <Button variant="outlined" onClick={() => setShowAnswer(true)}>
+                  Mostrar respuesta
+                </Button>
+              )}
+            </div>
 
             {showAnswer && (
-              <Typography variant="body2" sx={{ mt: 1 }}>
+              <Typography
+                variant="body2"
+                sx={{ mt: 1, mb: 2, textAlign: "center" }}
+              >
                 Respuesta: {correctAnswer}
               </Typography>
             )}
 
-            <DialogActions sx={{ mt: 2 }}>
-              <Button onClick={onCorrect}>Respondí bien</Button>
-              <Button onClick={onIncorrect}>Respondí mal</Button>
+            <DialogActions
+              className={styles.successErrorBox}
+              sx={{ justifyContent: "space-around", marginTop: 1 }}
+            >
+              <Button color="success" variant="contained" onClick={onCorrect}>
+                Correcto
+              </Button>
+              <Button color="error" variant="contained" onClick={onIncorrect}>
+                Incorrecto
+              </Button>
             </DialogActions>
-          </>
+          </div>
         )}
       </DialogContent>
     </Dialog>
